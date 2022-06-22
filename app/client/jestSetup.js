4bf1d8ee1c74f3fe812a19e2cd740efa633e1a98
Joe McIntyre <j_h_mcintyre@yahoo.com>
2022-06-22 12:16:20 -0500

    client: remove unused libraries, deps

--- app/client/jestSetup.js
+++
@@ -1,2 +0,0 @@
-/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
-import 'jest-styled-components';
--- app/client/libs/uikit/MenuSeparator.js
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-export const MenuSeparator = styled.hr`
-  margin: ${props => props.spacing || '4px'} 4px ${props => props.spacing || '4px'} 4px;
-`;

249ca08f7f8e54136ce1d8061cbf00edd3165054
Joe McIntyre <j_h_mcintyre@yahoo.com>
2022-06-23 11:10:39 -0500

    client: finish Typescript migration, removing JS Babel/Webpack

--- app/client/webpack.config.js
+++ app/client/webpack.config.js
@@ -22,15 +22,9 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
         test: /\.tsx?$/,
         loader: 'ts-loader',
+        exclude: /node_modules/,
         options: {
           compilerOptions: {
             noEmit: false,

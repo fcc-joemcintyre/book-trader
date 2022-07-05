4056f6df5e77f9859c6dbb7be227d65c31ec8f91
Joe McIntyre <j_h_mcintyre@yahoo.com>
2022-07-05 12:47:42 -0500

    lib: uikit add Pagination page ranges tests

--- lib/uikit/test/Pagination/Pagination.test.tsx
+++ lib/uikit/test/Pagination/Pagination.test.tsx
@@ -23,4 +23,28 @@ describe ('Pagination', () => {
     );
     expect (baseElement).toMatchSnapshot ();
   });
+  it ('subset of pages showing, starting at beginning', () => {
+    const { baseElement } = render (
+      <ThemeProvider theme={theme}>
+        <Pagination items={100} pageItems={10} visible={5} first={0} current={0} onChange={() => { /* no op */ }} />
+      </ThemeProvider>
+    );
+    expect (baseElement).toMatchSnapshot ();
+  });
+  it ('subset of pages showing, showing middle pages', () => {
+    const { baseElement } = render (
+      <ThemeProvider theme={theme}>
+        <Pagination items={100} pageItems={10} visible={5} first={2} current={2} onChange={() => { /* no op */ }} />
+      </ThemeProvider>
+    );
+    expect (baseElement).toMatchSnapshot ();
+  });
+  it ('subset of pages showing, showing lastpages', () => {
+    const { baseElement } = render (
+      <ThemeProvider theme={theme}>
+        <Pagination items={100} pageItems={10} visible={5} first={5} current={5} onChange={() => { /* no op */ }} />
+      </ThemeProvider>
+    );
+    expect (baseElement).toMatchSnapshot ();
+  });
 });

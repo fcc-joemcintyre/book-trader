aa652801db9dbb3290ec661e7f22e64d34ff0ad8
Joe McIntyre <j_h_mcintyre@yahoo.com>
2022-07-18 18:17:34 -0500

    lib uikit: Pagination initial test

--- lib/uikit/cypress/component/Pagination.cy.tsx
+++ lib/uikit/cypress/component/Pagination.cy.tsx
@@ -3,14 +3,15 @@ import './util/fix';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { mount } from 'cypress/react';
 import { ThemeProvider } from 'styled-components';
-import { Pagination } from '../../src/Pagination';
+import { Pagination } from '../../src';
 import { theme } from './util/theme';
 
 describe ('Pagination.cy.ts', () => {
-  it ('playground', () => {
+  it ('should show first five pages', () => {
     mount (
       <ThemeProvider theme={theme}>
         <Pagination
+          data-testid='pagination'
           items={100}
           pageItems={10}
           visible={5}
@@ -20,5 +21,12 @@ describe ('Pagination.cy.ts', () => {
         />
       </ThemeProvider>
     );
+    cy.get ('[data-testid="pagination"]').should ('contain', '1');
+    cy.get ('[data-testid="pagination"]').should ('contain', '2');
+    cy.get ('[data-testid="pagination"]').should ('contain', '3');
+    cy.get ('[data-testid="pagination"]').should ('contain', '4');
+    cy.get ('[data-testid="pagination"]').should ('contain', '5');
+    cy.get ('[data-testid="pagination"]').should ('contain', '>');
+    cy.get ('[data-testid="pagination"]').should ('not.contain', '6');
   });
 });
